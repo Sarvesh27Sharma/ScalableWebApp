84f573c1f20cd04b5230bc0eef67f8a8c415ecb1
sarvesh <sarvesh27sharma@gmail.com>
2021-01-18 22:17:19 +0100

    initial commit

---
+++ src/main/java/com/github/sarvesh27sharma/scalablewebapp/ScalableWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.sarvesh27sharma.scalablewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sarvesh27sharma/ScalableWebApp/ScalableWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sarvesh27sharma.ScalableWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
